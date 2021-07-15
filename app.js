185574e89d66b725894661ed2f17c40208f4049e
Pratham Mishra <prathammishra178@gmail.com>
2021-07-15 11:38:43 +0530

    update

---
+++ app.js
@@ -0,0 +1,102 @@
+let btn1 = document.querySelector('#btn-1');
+let btn2 = document.querySelector('#btn-2');
+let btn3 = document.querySelector('#btn-3');
+let btn4 = document.querySelector('#btn-4');
+let btn5 = document.querySelector('#btn-5');
+
+
+btn1.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.7)), url(../assets/header-bg.jpg)";
+    document.getElementById("btn-1").style.background='#ffffff';
+    document.getElementById("btn-1").style.height='14px';
+    document.getElementById("btn-1").style.width='14px';
+    document.getElementById("btn-2").style.background='#737373';
+    document.getElementById("btn-2").style.height='12px';
+    document.getElementById("btn-2").style.width='12px';
+    document.getElementById("btn-3").style.background='#737373';
+    document.getElementById("btn-3").style.height='12px';
+    document.getElementById("btn-3").style.width='12px';
+    document.getElementById("btn-4").style.background='#737373';
+    document.getElementById("btn-4").style.height='12px';
+    document.getElementById("btn-4").style.width='14px';
+    document.getElementById("btn-5").style.background='#737373';
+    document.getElementById("btn-5").style.height='12px';
+    document.getElementById("btn-5").style.width='12px';
+});
+
+btn2.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.70), rgba(0, 0, 0, 0.700)), url('../assets/bg1.jpg')";
+    document.getElementById("btn-1").style.background='#737373';
+    document.getElementById("btn-1").style.height='12px';
+    document.getElementById("btn-1").style.width='12px';
+    document.getElementById("btn-2").style.background='#ffffff';
+    document.getElementById("btn-2").style.height='14px';
+    document.getElementById("btn-2").style.width='14px';
+    document.getElementById("btn-3").style.background='#737373';
+    document.getElementById("btn-3").style.height='12px';
+    document.getElementById("btn-3").style.width='12px';
+    document.getElementById("btn-4").style.background='#737373';
+    document.getElementById("btn-4").style.height='12px';
+    document.getElementById("btn-4").style.width='12px';
+    document.getElementById("btn-5").style.background='#737373';
+    document.getElementById("btn-5").style.height='12px';
+    document.getElementById("btn-5").style.width='12px';
+});
+
+btn3.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.70), rgba(0, 0, 0, 0.700)), url('../assets/bg2.jpg')";
+    document.getElementById("btn-1").style.background='#737373';
+    document.getElementById("btn-1").style.height='12px';
+    document.getElementById("btn-1").style.width='12px';
+    document.getElementById("btn-2").style.background='#737373';
+    document.getElementById("btn-2").style.height='12px';
+    document.getElementById("btn-2").style.width='12px';
+    document.getElementById("btn-3").style.background='#ffffff';
+    document.getElementById("btn-3").style.height='14px';
+    document.getElementById("btn-3").style.width='14px';
+    document.getElementById("btn-4").style.background='#737373';
+    document.getElementById("btn-4").style.height='12px';
+    document.getElementById("btn-4").style.width='12px';
+    document.getElementById("btn-5").style.background='#737373';
+    document.getElementById("btn-5").style.height='12px';
+    document.getElementById("btn-5").style.width='12px';
+});
+
+btn4.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.70), rgba(0, 0, 0, 0.700)), url('../assets/bg3.jpg')";
+    document.getElementById("btn-1").style.background='#737373';
+    document.getElementById("btn-1").style.height='12px';
+    document.getElementById("btn-1").style.width='12px';
+    document.getElementById("btn-2").style.background='#737373';
+    document.getElementById("btn-2").style.height='12px';
+    document.getElementById("btn-2").style.width='12px';
+    document.getElementById("btn-3").style.background='#737373';
+    document.getElementById("btn-3").style.height='12px';
+    document.getElementById("btn-3").style.width='12px';
+    document.getElementById("btn-4").style.background='#ffffff';
+    document.getElementById("btn-4").style.height='14px';
+    document.getElementById("btn-4").style.width='14px';
+    document.getElementById("btn-5").style.background='#737373';
+    document.getElementById("btn-5").style.height='12px';
+    document.getElementById("btn-5").style.width='12px';
+});
+
+btn5.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.70), rgba(0, 0, 0, 0.700)), url('../assets/bg4.jpg')";
+    document.getElementById("btn-1").style.background='#737373';
+    document.getElementById("btn-1").style.height='12px';
+    document.getElementById("btn-1").style.width='12px';
+    document.getElementById("btn-2").style.background='#737373';
+    document.getElementById("btn-2").style.height='12px';
+    document.getElementById("btn-2").style.width='12px';
+    document.getElementById("btn-3").style.background='#737373';
+    document.getElementById("btn-3").style.height='12px';
+    document.getElementById("btn-3").style.width='12px';
+    document.getElementById("btn-4").style.background='#737373';
+    document.getElementById("btn-4").style.height='12px';
+    document.getElementById("btn-4").style.width='12px';
+    document.getElementById("btn-5").style.background='#ffffff';
+    document.getElementById("btn-5").style.height='14px';
+    document.getElementById("btn-5").style.width='14px';
+});
+
